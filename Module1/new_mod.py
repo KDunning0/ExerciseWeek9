fd0f998a01980760a1960fc294695e1a43624078
Katie Dunning <umu5hh@virginia.edu>
2025-03-18 20:19:56 -0400

    Adding participation week 9

---
+++ Module1/new_mod.py
@@ -0,0 +1,3 @@
+def say_something():
+    """A confused module"""
+    print("Well, what do you want me to say?")
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+name='Demo',
+version='0.1',
+description='My participation package',
+url='https://github.com/KDunning0/ExerciseWeek9.git',
+author='umu5hh',
+packages=['Module1']
+)
+
